1db8afd5d6df427c24deb0d7b3fa0b0c5efc2d0b
Simon Conrad <simondbconrad@gmail.com>
2024-06-17 16:21:12 -0400

    enable redux dev tools for store

--- redux-js/src/store.js
+++ redux-js/src/store.js
@@ -1,6 +1,8 @@
-import { createStore, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
-import { sayHiOnDispatch, includeMeaningOfLife } from './addons/enhancers'
+// import { sayHiOnDispatch, includeMeaningOfLife } from './addons/enhancers'
+import { composeWithDevTools } from '@redux-devtools/extension'
+import { print1, print2, print3 } from './addons/middleware'
 
 let preloadedState
 const persistedTodosString = localStorage.getItem('todos')
@@ -11,7 +13,18 @@ if (persistedTodosString) {
   }
 }
 
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+const composedEnhancer = composeWithDevTools(
+    applyMiddleware(print1, print2, print3)
+)
+
+// const loggerMiddleware = storeAPI => next => action => {
+//     console.log('dispatching', action)
+//     let result = next(action)
+//     console.log('next state', storeAPI.getState())
+//     return result
+// }
+
+// const middlewareEnhancer = applyMiddleware(loggerMiddleware)
 
 const store = createStore(rootReducer, preloadedState, composedEnhancer)
 
